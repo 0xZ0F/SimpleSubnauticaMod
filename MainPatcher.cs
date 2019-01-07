21e5aebbab387b30742a941b44d754ec60720284
Z0F <kgoforth07@gmail.com>
2019-01-07 17:07:19 -0600

    Add files via upload

---
+++ MainPatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+
+// You can use this file almost as-is. Just change the marked lines below. This will be the main file of each mod that tells Harmony to load your changes.
+namespace SubnauticaModTest1     // Change this line to match your mod.
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.z0f.subnautica.test.mod");   // Change this line to match your mod. 
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ Player_Update_Patch.cs
@@ -0,0 +1,44 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace SubnauticaModTest1
+{
+    [HarmonyPatch(typeof(Player))]
+    [HarmonyPatch("Update")]
+    internal class Player_Update_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                IngameMenu.main.SaveGame();
+                IngameMenu.main.QuitSubscreen(); // Previous call can cause a 'ghost menu' to be brought up and invisible. This closes it.
+
+            }
+        }
+
+        [HarmonyPostfix]
+        public static void Heal()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Player.main.liveMixin.ResetHealth();
+            }
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+
+    [HarmonyPatch(typeof(LiveMixin))]
+    [HarmonyPatch("TakeDamage")]
+    internal class Player_Health_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Preifx()
+        {
+            Player.main.liveMixin.ResetHealth();
+            return false;   // This will also prevent any FX - Use Postfix if you don't want this.
+        }
+    }
+}
